4be9cf865c256592108b88ea71335c614f7094b7
MarcoCaceresSenn <m.caceres04@ufromail.cl>
2024-10-11 12:53:40 -0300

    build: agregacion metodo filtrarImagenes vacio

--- src/servicios/busqueda/ServicioBusqueda.ts
+++ src/servicios/busqueda/ServicioBusqueda.ts
@@ -1,3 +1,6 @@
 class ServicioBusqueda {
+    filtrarImagenes(jsonCompleto: { imagenesNoFiltradas: { id: number; url: string; formato: string; resolucion: string; etiquetas: string[]; titulo: string; }[]; }, parametroDeBusqueda: string) {
+        throw new Error("Method not implemented.");
+    }
 
 }
