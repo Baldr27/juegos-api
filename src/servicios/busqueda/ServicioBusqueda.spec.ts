e795b1b20a836898d45319ba75292c80ce7f527e
MarcoCaceresSenn <m.caceres04@ufromail.cl>
2024-10-11 12:49:37 -0300

    build: mocks de test para imágenes
    Se agregaron 2 mocks, uno con una lista completa de imágenes y otro con una parte para testear el filtro de busqueda, CON TDD

---
+++ src/servicios/busqueda/ServicioBusqueda.spec.ts
@@ -0,0 +1,17 @@
+import { expect } from "chai";
+import "mocha";
+import imagenesFiltradas from "../../mocks/imagenesFiltradas.json";
+import imagenesNoFiltradas from "../../mocks/imagenesNoFiltradas.json";
+
+describe("Pruebas de servicios de Busqueda", () => {
+    it("Deberia retornar una json con una lista de imágenes que contengan el parámetro de búsqueda", () => {
+        const busqueda = new ServicioBusqueda();
+
+        const jsonEsperado = imagenesFiltradas;
+        const jsonCompleto = imagenesNoFiltradas;
+
+        const filtradas = busqueda.filtrarImagenes(jsonCompleto);
+
+        expect(jsonEsperado).to.equal(filtradas);
+    });
+});
