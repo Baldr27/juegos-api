bc4d30c699e1b22626dcc9471db25afd3338498e
GERMAN PATRICIO JARA AGUILERA <g.jara08@ufromail.cl>
2024-10-14 09:48:36 -0300

    added allure for test reports. fixed ServicioBusqueda error

--- src/servicios/busqueda/ServicioBusqueda.spec.ts
+++ src/servicios/busqueda/ServicioBusqueda.spec.ts
@@ -2,6 +2,7 @@ import { expect } from "chai";
 import "mocha";
 import imagenesFiltradas from "../../mocks/imagenesFiltradas.json";
 import imagenesNoFiltradas from "../../mocks/imagenesNoFiltradas.json";
+import { ServicioBusqueda } from './ServicioBusqueda.ts';
 
 describe("Pruebas de servicios de Busqueda", () => {
     it("Deberia retornar una json con una lista de imágenes que contengan el parámetro de búsqueda", () => {
--- src/servicios/busqueda/ServicioBusqueda.ts
+++ src/servicios/busqueda/ServicioBusqueda.ts
@@ -1,4 +1,4 @@
-class ServicioBusqueda {
+export class ServicioBusqueda {
     filtrarImagenes(jsonCompleto: { imagenesNoFiltradas: { id: number; url: string; formato: string; resolucion: string; etiquetas: string[]; titulo: string; }[]; }, parametroDeBusqueda: string) {
         throw new Error("Method not implemented.");
     }
