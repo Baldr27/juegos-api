6b84f18322a567ab76fe1d75d547351079aa30ad
GERMAN PATRICIO JARA AGUILERA <g.jara08@ufromail.cl>
2024-10-14 13:49:01 -0300

    changes to tests

--- src/servicios/busqueda/ServicioBusqueda.ts
+++ src/servicios/busqueda/ServicioBusqueda.ts
@@ -1,6 +1,8 @@
-export class ServicioBusqueda {
+class ServicioBusqueda {
     filtrarImagenes(jsonCompleto: { imagenesNoFiltradas: { id: number; url: string; formato: string; resolucion: string; etiquetas: string[]; titulo: string; }[]; }, parametroDeBusqueda: string) {
         throw new Error("Method not implemented.");
     }
 
 }
+
+export const servicioBusqueda = new ServicioBusqueda();
